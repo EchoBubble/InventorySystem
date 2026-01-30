e0eaa4cf67b81f75292f71d919fa3e4da5f0424f
EchoBubble <1756865212@qq.com>
2026-01-30 16:11:03 +0800

    The HUD Widget

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/Inv_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Inventory.h"
+#include "Blueprint/UserWidget.h"
+#include "Widgets/HUD/Inv_HUDWidget.h"
+
+AInv_PlayerController::AInv_PlayerController()
+{
+	
+}
+
+void AInv_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogInventory, Log, TEXT("BeginPlay for PlayerController"));
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		for (UInputMappingContext* CurrentContext : DefaultIMCs)
+		{
+			Subsystem->AddMappingContext(CurrentContext, 0);
+		}
+	}
+	CreateHUDWidget();
+}
+
+void AInv_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EIC = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EIC->BindAction(PrimaryInteractAction, ETriggerEvent::Started, this, &AInv_PlayerController::PrimaryInteract);
+}
+
+void AInv_PlayerController::PrimaryInteract()
+{
+	UE_LOG(LogInventory, Warning, TEXT("Primary Interact"));
+}
+
+void AInv_PlayerController::CreateHUDWidget()
+{
+	if (!IsLocalController()) return;
+
+	HUDWidget = CreateWidget<UInv_HUDWidget>(this,HUDWidgetClass);
+	if (IsValid(HUDWidget))
+	{
+		HUDWidget->AddToViewport();
+	}
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Inv_PlayerController.generated.h"
+
+/**
+ * 
+ */
+
+class UInv_HUDWidget;
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS(PrioritizeCategories=("Inventory"))
+class INVENTORY_API AInv_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	AInv_PlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void SetupInputComponent() override;
+private:
+	void PrimaryInteract();
+	void CreateHUDWidget();
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TArray<TObjectPtr<UInputMappingContext>> DefaultIMCs;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TObjectPtr<UInputAction> PrimaryInteractAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TSubclassOf<UInv_HUDWidget> HUDWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UInv_HUDWidget> HUDWidget;
+};
