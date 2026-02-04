e471d3e6dc457b399ca511e5c0c925e0ee74d392
EchoBubble <1756865212@qq.com>
2026-02-04 14:28:04 +0800

    Switcher Buttons

--- Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
@@ -6,6 +6,7 @@
 #include "Widgets/Inventory/InventoryBase/Inv_InventoryBase.h"
 #include "Inv_SpatialInventory.generated.h"
 
+class UButton;
 class UInv_InventoryGrid;
 class UWidgetSwitcher;
 /**
@@ -16,11 +17,16 @@ class INVENTORY_API UInv_SpatialInventory : public UInv_InventoryBase
 {
 	GENERATED_BODY()
 
+public:
+
+	virtual void NativeOnInitialized() override;
 private:
 
+	/* BindWidget 蓝图上创建，且命名必须一致*/
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UWidgetSwitcher> Switcher;
 
+	/* Grid Widget*/
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UInv_InventoryGrid> Grid_Equippables;
 	
@@ -29,4 +35,29 @@ private:
 
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UInv_InventoryGrid> Grid_Craftables;
+
+	/* Button */
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Equippables;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Consumables;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Craftables;
+
+	/* Callback Function*/
+	UFUNCTION()
+	void ShowEquippables();
+
+	UFUNCTION()
+	void ShowConsumables();
+
+	UFUNCTION()
+	void ShowCraftables();
+	/* End Callback*/
+	
+	void DisableButton(UButton* Button);
+
+	void SetActiveGrid(UInv_InventoryGrid* Grid, UButton* Button);
 };
