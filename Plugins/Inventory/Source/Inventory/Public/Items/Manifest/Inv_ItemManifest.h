7cb4eab64990a7b15a696c98213a243eda4bab3a
EchoBubble <1756865212@qq.com>
2026-02-12 16:37:54 +0800

    Item Type Tag

--- Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
@@ -3,7 +3,9 @@
 #include "CoreMinimal.h"
 #include "Types/Inv_GridTypes.h"
 #include "StructUtils/InstancedStruct.h"
+#include "GameplayTagContainer.h"
 #include "Inv_ItemManifest.generated.h"
+
 
 /*
  *  Item Manifest 包含创建新的 Inventory Item 的所有必需数据
@@ -16,10 +18,15 @@ struct INVENTORY_API FInv_ItemManifest
 	
 	UInv_InventoryItem* Manifest(UObject* NewOuter);
 	EInv_ItemCategory GetItemCategory() const { return ItemCategory; }
+	FGameplayTag GetItemType() const {return ItemType; }
+	
 private:
 
 	UPROPERTY(EditAnywhere, Category = "Inventory")
 	EInv_ItemCategory ItemCategory{EInv_ItemCategory::None};
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	FGameplayTag ItemType;
 };
 
 
